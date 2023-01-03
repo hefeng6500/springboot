d271b42ff2adf695144c4386c5617afb1d814db0
hefeng6500 <2443992009@qq.com>
2023-01-03 21:46:58 +0800

    feat: springboot 整合 junit

---
+++ src/test/java/com/yang/springboot/Springboot07TestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.yang.springboot;
+
+import com.yang.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot07TestApplicationTests {
+
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    public void save() {
+        bookService.save();
+    }
+}
