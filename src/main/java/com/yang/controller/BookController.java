37aa2b8614da95e2e6d015c7353fac568d37ece7
hefeng6500 <2443992009@qq.com>
2023-01-03 20:44:16 +0800

    feat: yml 配置文件变量读取

--- src/main/java/com/yang/controller/BookController.java
+++ src/main/java/com/yang/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.yang.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/books")
 public class BookController {
 
+    @Value("${lesson}")
+    private String lesson;
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @Value("${enterprise.subject[0]}")
+    private String subject_00;
+
     @GetMapping("/{id}")
     public String getById(@PathVariable Integer id){
         System.out.println("id ==> "+id);
+        System.out.println(lesson);
+        System.out.println(port);
+        System.out.println(subject_00);
         return "<h1>hello , spring boot!</h1>";
     }
 }
