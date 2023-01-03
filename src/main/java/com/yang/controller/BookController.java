715416b76ddb29168c717530af12bf18fcf14c1d
hefeng6500 <2443992009@qq.com>
2023-01-03 20:47:09 +0800

    feat: yml 配置文件变量通过 Environment 读取

--- src/main/java/com/yang/controller/BookController.java
+++ src/main/java/com/yang/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.yang.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,21 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/books")
 public class BookController {
 
-    @Value("${lesson}")
-    private String lesson;
+//    @Value("${lesson}")
+//    private String lesson;
+//
+//    @Value("${server.port}")
+//    private Integer port;
+//
+//    @Value("${enterprise.subject[0]}")
+//    private String subject_00;
 
-    @Value("${server.port}")
-    private Integer port;
-
-    @Value("${enterprise.subject[0]}")
-    private String subject_00;
+    @Autowired
+    private Environment env;
 
     @GetMapping("/{id}")
     public String getById(@PathVariable Integer id){
         System.out.println("id ==> "+id);
-        System.out.println(lesson);
-        System.out.println(port);
-        System.out.println(subject_00);
+        System.out.println(env.getProperty("lesson"));
+        System.out.println(env.getProperty("server.port"));
+        System.out.println(env.getProperty("enterprise.subject[0]"));
+//        System.out.println(lesson);
+//        System.out.println(port);
+//        System.out.println(subject_00);
         return "<h1>hello , spring boot!</h1>";
     }
 }
